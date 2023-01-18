690c300caa28ab39ae4ae479d5151547e0b6285e
Alexfp05405 <alexdislva79@gmail.com>
2023-01-18 21:56:21 +0530

    update

--- Routes/product.route.js
+++ Routes/product.route.js
@@ -12,8 +12,33 @@ ProductRouter.post("/add",async(req,res)=>{
     }
     catch(err){
         console.log(err)
-        res.send("something wrong while posting")
+        res.send("something wrong while creating product")
     }
+})
+
+ProductRouter.get("/",async(req,res)=>{
+    try{
+        const data=await ProductModel.find()
+        res.send(data)
+    }
+    catch(err){
+        console.log(err)
+        res.send("something wrong while getting products")
+    }
+})
+
+ProductRouter.patch("/update/:id",async(req,res)=>{
+    const {id}=req.params
+    const payload=req.body
+    try{
+        await PostModel.findByIdAndUpdate({_id:id},payload,{new:true, runValidators:true, useFindAndModify:false})
+        res.send(`post upadted with the id ${id}`)
+    }
+    catch(err){
+        console.log(err)
+        res.send("something wrong while updating your posts")
+    }
+
 })
 
 
