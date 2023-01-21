037cfd36eeb8798ad73e2a8185b4e9b55f9fd2eb
Pushpendra Singh <erpushpendrasingh8900@gmail.com>
2023-01-21 15:29:11 +0530

    Backend Filter by brand and  name, adding data in bulk function

--- Routes/product.route.js
+++ Routes/product.route.js
@@ -1,46 +1,108 @@
-const express=require("express")
-const {ProductModel}=require("../model/product.model")
-const ProductRouter=express.Router()
+const express = require("express");
+const { products } = require("../constants/data");
+const { ProductModel } = require("../model/product.model");
+const ProductRouter = express.Router();
 
+ProductRouter.post("/add", async (req, res) => {
+     const data = req.body;
+     try {
+          const user = new ProductModel(data);
+          await user.save();
+          res.status(201).send({ msg: "Created SUCCESSFULLY" });
+     } catch (err) {
+          console.log(err);
+          res.status(404).send({
+               msg: "something wrong while creating product",
+          });
+     }
+});
 
-ProductRouter.post("/add",async(req,res)=>{
-    const data=req.body
-    try{
-        const user=new ProductModel(data)
-        await user.save()
-        res.send("successfully added")
-    }
-    catch(err){
-        console.log(err)
-        res.send("something wrong while creating product")
-    }
-})
+ProductRouter.get("/", async (req, res) => {
+     try {
+          console.log(req.body);
+          let {
+               productname,
+               productBrand,
+               productCategory,
+               sortBy,
+               limit = 50,
+               page = 1,
+          } = req.query;
+          let queries = {};
+          if (
+               productname === undefined &&
+               productBrand === undefined &&
+               productCategory === undefined
+          ) {
+               queries = {};
+          } else if (productname === undefined) {
+               queries.productBrand = { $regex: productBrand, $options: "i" };
+          } else if (productBrand === undefined) {
+               queries.productname = { $regex: productname, $options: "i" };
+          } else if (productname === undefined && productBrand === undefined) {
+               queries.productBrand = { $regex: productBrand, $options: "i" };
+          } else if (productBrand === undefined) {
+               queries.productname = { $regex: productname, $options: "i" };
+          } else {
+               queries.productBrand = { $regex: productBrand, $options: "i" };
+               queries.productname = { $regex: productname, $options: "i" };
+          }
 
-ProductRouter.get("/",async(req,res)=>{
-    try{
-        const data=await ProductModel.find()
-        res.send(data)
-    }
-    catch(err){
-        console.log(err)
-        res.send("something wrong while getting products")
-    }
-})
+          let sorting = {};
+          if (sortBy != undefined) {
+               sorting[sortBy] = 1;
+          }
 
-ProductRouter.patch("/update/:id",async(req,res)=>{
-    const {id}=req.params
-    const payload=req.body
-    try{
-        await ProductModel.findByIdAndUpdate({_id:id},payload,{new:true, runValidators:true, useFindAndModify:false})
-        res.send(`post upadted with the id ${id}`)
-    }
-    catch(err){
-        console.log(err);
+          let products = await ProductModel.find(queries)
+               .sort(sorting)
+               .skip((page - 1) * limit)
+               .limit(limit);
+          const totalCount = products?.length;
+          res.status(200).json({ data: products, totalCount });
+     } catch (err) {
+          console.log(err);
+          res.status(404).send({
+               msg: "something wrong while getting products",
+          });
+     }
+});
 
-        res.send("something wrong while updating your posts")
-    }
+ProductRouter.get("/singleProduct/:id", async (req, res) => {
+     const { id } = req.params;
+     try {
+          const data = await ProductModel.findById({ _id: id });
+          res.status(200).json({ data });
+     } catch (err) {
+          console.log(err);
+          res.status(404).send({
+               msg: "something wrong while getting products",
+          });
+     }
+});
 
-})
+ProductRouter.patch("/update/:id", async (req, res) => {
+     const { id } = req.params;
+     const payload = req.body;
+     try {
+          await ProductModel.findByIdAndUpdate({ _id: id }, payload, {
+               new: true,
+               runValidators: true,
+               useFindAndModify: false,
+          });
+          //  res.send(`post upadted with the id ${id}`);
+          res.status(200).send({ msg: "Updated SUCCESSFULLY" });
+     } catch (err) {
+          console.log(err);
+          res.status(404).send({ msg: "FAILED TO UPDATE THE DATA" });
+     }
+});
+const addBulkDataManually = async () => {
+     try {
+          await ProductModel.insertMany(products);
+          console.log("Bulk Data Uploaded SUCCESSFULLY");
+     } catch (error) {
+          console.log(error, "FAILED TO Upload Bulk Data");
+     }
+};
 
-
-module.exports={ProductRouter}
+module.exports = { ProductRouter, addBulkDataManually };
